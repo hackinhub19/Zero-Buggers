cdd613ce9398e6de1fe1a9552e76dd465a79d9b8
Zero Buggers <vijayaalayan1234@gmail.com>
2019-09-13 03:24:02 +0530

    Generating as many questions as possible for the file db

---
+++ main.py
@@ -0,0 +1,26 @@
+import aqgFunction
+
+
+# Main Function
+def main():
+    # Create AQG object
+    aqg = aqgFunction.AutomaticQuestionGenerator()
+
+    inputTextPath = "C:/Users/DELL/Desktop/sairam_main_project/DB/db.txt"
+    readFile = open(inputTextPath, 'r+')
+    #readFile = open(inputTextPath, 'r+', encoding="utf8", errors = 'ignore')
+
+    inputText = readFile.read()
+
+    questionList = aqg.aqgParse(inputText)
+    aqg.display(questionList)
+
+    #aqg.DisNormal(questionList)
+
+    return 0
+
+
+# Call Main Function
+if __name__ == "__main__":
+    main()
+
